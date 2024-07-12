80425bdf7500807b3eb867727f57bd9a7e35e6c6
Yan <timbrist@outlook.com>
2024-07-12 15:32:25 +0300

    update the download

--- download.sh
+++ download.sh
@@ -3,12 +3,13 @@
 # export WORKSPACE=/projappl/project_2010633/RAG-Driver
 export WORKSPACE=$(pwd)
 
+# Please change this directory to save the models on wherever you want. 
+export MODELS_DIR=${WORKSPACE}/models
 
-export VIDEO_LLAVA=${WORKSPACE}/models/Video-LLaVA-7B
-export VIDEO_LLAVA_PRETRAIN=${WORKSPACE}/models/Video-LLaVA-Pretrain-7B
-export LANGUAGEBIN_VIDEO=${WORKSPACE}/models/LanguageBind_Video_merge
-export LANGUAGEBIN_IMAGE=${WORKSPACE}/models/LanguageBind_Image
-
+export VIDEO_LLAVA=${MODELS_DIR}/Video-LLaVA-7B
+export VIDEO_LLAVA_PRETRAIN=${MODELS_DIR}/Video-LLaVA-Pretrain-7B
+export LANGUAGEBIN_VIDEO=${MODELS_DIR}/LanguageBind_Video_merge
+export LANGUAGEBIN_IMAGE=${MODELS_DIR}/LanguageBind_Image
 
 #Download the checkpoint from hugginface without large file
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/LanguageBind/Video-LLaVA-7B ${VIDEO_LLAVA}
@@ -16,27 +17,29 @@ GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/LanguageBind/Video-LLaVA-
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/LanguageBind/LanguageBind_Video_merge ${LANGUAGEBIN_VIDEO}
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/LanguageBind/LanguageBind_Image ${LANGUAGEBIN_IMAGE}
 
+sleep 3s # Waits 3 seconds.
+
 # download video llava large file using wget manually
 wget https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/tokenizer.model?download=true \
-    O ${VIDEO_LLAVA}/tokenizer.model
+    -O ${VIDEO_LLAVA}/tokenizer.model 
 
 wget https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/model-00001-of-00002.safetensors?download=true \
-    -O ${VIDEO_LLAVA}/model-00001-of-00002.safetensors \
+    -O ${VIDEO_LLAVA}/model-00001-of-00002.safetensors 
 
 wget https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/model-00002-of-00002.safetensors?download=true \
-    -O ${VIDEO_LLAVA}/model-00002-of-00002.safetensors \
+    -O ${VIDEO_LLAVA}/model-00002-of-00002.safetensors 
 
 wget https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/pytorch_model-00001-of-00002.bin?download=true \
-    -O ${VIDEO_LLAVA}/pytorch_model-00001-of-00002.bin \
+    -O ${VIDEO_LLAVA}/pytorch_model-00001-of-00002.bin 
 
 wget https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/pytorch_model-00002-of-00002.bin?download=true \
-    -O ${VIDEO_LLAVA}/pytorch_model-00002-of-00002.bin \
+    -O ${VIDEO_LLAVA}/pytorch_model-00002-of-00002.bin 
 
 wget https://huggingface.co/LanguageBind/Video-LLaVA-Pretrain-7B/resolve/main/mm_projector.bin?download=true \
-    -O ${VIDEO_LLAVA_PRETRAIN}/Video-LLaVA-Pretrain-7B/mm_projector.bin \
+    -O ${VIDEO_LLAVA_PRETRAIN}/Video-LLaVA-Pretrain-7B/mm_projector.bin 
 
 wget https://huggingface.co/LanguageBind/LanguageBind_Video_merge/resolve/main/pytorch_model.bin?download=true \
-    -O ${LANGUAGEBIN_VIDEO}/LanguageBind_Video_merge/pytorch_model.bin \
+    -O ${LANGUAGEBIN_VIDEO}/LanguageBind_Video_merge/pytorch_model.bin 
 
 wget https://huggingface.co/LanguageBind/LanguageBind_Image/resolve/main/pytorch_model.bin?download=true \
     -O ${LANGUAGEBIN_IMAGE}/LanguageBind_Image/pytorch_model.bin
