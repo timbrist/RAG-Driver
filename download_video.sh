4e7ebd2b2b9c409fcab66e65f4180f0d84f71616
Yan <timbrist@outlook.com>
2024-07-12 16:12:29 +0300

    download video

---
+++ download_video.sh
@@ -0,0 +1,6 @@
+
+export WORKSPACE=$(pwd)
+
+wget --no-check-certificate \
+    'https://drive.usercontent.google.com/download?id=14a3QTkWRelAZs-kW_2U5tjYcAm2l8VbF&export=download&authuser=0&confirm=t&uuid=42f173e7-d4f3-4ee4-a88c-e1bdfb531b69&at=APZUnTUOSlPhljxTnnWF6E0Rn1bd%3A1720179449276' \
+    -O ${WORKSPACE}/video_process/video_process.tar.gz
